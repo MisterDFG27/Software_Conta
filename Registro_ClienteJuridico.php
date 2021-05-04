bdb135fb0175fe9f6cdeff483b07c7fef3728cf1
Stacy428 <artstacy@hotmail.com>
2021-05-03 19:17:37 -0600

    quite la o

--- Registro_ClienteJuridico.php
+++ Registro_ClienteJuridico.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Registro Cliente Jurídicoo</title>
+    <title>Registro Cliente Jurídico</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
