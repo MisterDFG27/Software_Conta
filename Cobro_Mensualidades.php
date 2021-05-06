eef163e4b23b464c6a9ba409a37531f3a8e22773
MisterDFG27 <dfgtoledo27@gmail.com>
2021-05-05 23:19:59 -0600

    listo frames

---
+++ Cobro_Mensualidades.php
@@ -0,0 +1,14 @@
+<?php include("conexion.php");  ?>
+
+<?php include('includes/header.php');  ?>
+
+<!---------------------------------------------------------------- INICIO      COLLAPSE --------------------------------------------------->
+<br>
+
+<h2 align="center">Cobro Mensualidades</h2>
+
+<br>
+<br>
+<br>
+
+<?php include('includes/footer.php');  ?>
